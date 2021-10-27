48925029dc16f3c56e6cdb735a06d4a11be479bd
Denis FOX <dfox@foxartbox.com>
2021-10-28 01:08:52 +0300

    FAQ. v7

--- assets/theme.js
+++ assets/theme.js
@@ -9695,6 +9695,20 @@ $(window).bind('load', function() {
 	$('.js-header-menu-toggle').click(function () {
 		$('html').toggleClass('header_menu_active');
 	});
+
+	$('.js-block-show').click(function () {
+		var blockId = $(this).attr('data-block');
+
+		$(this).addClass('active');
+		$('.js-block[data-block="' + blockId + '"]').addClass('active');
+	});
+
+	$('.js-block-hide').click(function () {
+		var blockId = $(this).attr('data-block');
+
+		$('.js-block-show[data-block="' + blockId + '"]').removeClass('active');
+		$('.js-block[data-block="' + blockId + '"]').removeClass('active');
+	});
 });
 
 
