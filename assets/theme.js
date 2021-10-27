940bb1cbb8807d97c7f03450379e08dd7b659daa
Denis FOX <dfox@foxartbox.com>
2021-10-28 01:22:34 +0300

    FAQ. v12

--- assets/theme.js
+++ assets/theme.js
@@ -9706,8 +9706,8 @@ $(window).bind('load', function() {
 	$('.js-block-hide').click(function () {
 		var blockId = $(this).attr('data-block');
 
-		$('.js-block-toggle[data-block="' + blockId + '"]').removeClass('active');
 		$('.js-block-show[data-block="' + blockId + '"]').removeClass('active');
+		$('.js-block-toggle[data-block="' + blockId + '"]').removeClass('active');
 		$('.js-block[data-block="' + blockId + '"]').removeClass('active');
 	});
 
